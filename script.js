6004f19a9c92ec3aaf5b318ba7cc82eae3ff84c2
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-25 11:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
+	const firstname = document.querySelector('[name="fname"]').value;
+	const lastname = document.querySelector('[name="lname"]').value;
+
+	alert(firstname+ " " + lastname);
+	
     //Write your code here
 
 }
